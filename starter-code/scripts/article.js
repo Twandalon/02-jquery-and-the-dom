d2ded595e72e2ee55838d182f1f3119d090c250f
Austin Cohrs <acohrsdev@outlook.com>
2017-07-11 18:57:13 -0700

    Completed toHtml function.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,7 +1,5 @@
 'use strict';
-
 var articles = [];
-
 function Article (rawDataObj) {
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
@@ -10,18 +8,16 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
 }
-
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.removeClass();
+  $newArticle.addClass('name');
   /* TODO: This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
-
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
-
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest
   of the current template clone with properties from this particular Article instance.
   We need to fill in:
@@ -30,23 +26,24 @@ Article.prototype.toHtml = function() {
     3. article title,
     4. article body, and
     5. publication date. */
-
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href',this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('section').html(this.body);
+  $newArticle.find('time').attr('datetime',this.publishedOn);
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
-
 rawData.sort(function(a,b) {
   // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
-
 rawData.forEach(function(articleObject) {
   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
   articles.push(new Article(articleObject));
 });
-
 articles.forEach(function(article) {
   $('#articles').append(article.toHtml());
 });
